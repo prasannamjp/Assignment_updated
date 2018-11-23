993fbc934a75a0d50c88a5b9be40a1a84f2a8d82
prasannamj <prasannamj23@gmail.com>
2018-11-23 11:14:02 +0530

    Modified records.py with comments

--- records.py
+++ records.py
@@ -14,18 +14,27 @@ class Restaurant:
     regx_3 = r'\w[a-z A-z]{2,20}'
 
     def total_orders(self):
+        '''
+            Method to check how many orders did the site receive.
+        '''
         orders = re.findall(self.regx_1, data)
         count = len(orders)
         return count
 
     def total_order_amount(self):
-            Amount = re.findall(self.regx_2, data)
-            total = 0
-            for i in Amount:
-                total += (int(i))
-            return total
+        '''
+            Method to calculate the total amount of the orders.
+        '''        
+        Amount = re.findall(self.regx_2, data)
+        total = 0
+        for i in Amount:
+            total += (int(i))
+        return total
 
     def customers_count(self):
+        '''
+            Method to find the number of times the customer placed an order.  
+        '''
         names = re.findall(self.regx_3, data)
         wordcount = {}
         for word in names:
@@ -36,7 +45,11 @@ class Restaurant:
         return wordcount
 
     def once_ordered(self):
-        wordcount = self.customers_count()
+        '''
+            Method to List the names of the customers who
+            ordered once and did not order again.
+        '''
+        wordcount = self.customers_count()      # calling customers_count() method   
         count = 0
         names = []
         for key, value in wordcount.items():
@@ -46,6 +59,11 @@ class Restaurant:
         return count, names
 
     def customer_report(self):
+        '''
+            Method to Get a distribution of customers who ordered exactly once,
+            exactly twice and so on up to 4 orders and
+            group the rest as 5 orders and above.
+        '''
         orders = {}
         wordcount = self.customers_count()
         order_count = [1, 2, 3, 4, 5, 6, 7, 31]
